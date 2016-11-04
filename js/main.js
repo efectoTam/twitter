a70fb64e28d86f409f658b3b681a5854f0271da4
efectoTam <tam.luque@gmail.com>
2016-11-04 13:00:38 -0300

    Agregado el checkbox

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 function agregarTarea(){
-	contenedorDeTareas = document.getElementsByClassName("tarea")[0];
+
+  contenedorDeTareas = document.getElementsByClassName("tarea")[0];
 	nuevaTarea = document.createElement("div");
 	tareaUsuario = document.getElementsByTagName("textarea")[0].value;
-  	contenedorNuevaTarea = document.createElement("p");
-  	contenedorNuevaTarea.setAttribute("class", "pDeTarea");
-  	tarroBasura = document.createElement("span");
-  	tarroBasura.setAttribute("class", "icon-bin basurero");
-  	//tarroBasura.setAttribute("onclick", borrarTareas()) //verificar nombre de funcion
-  	nodoTarea = document.createTextNode(tareaUsuario);
-  	contenedorNuevaTarea.appendChild(nodoTarea);
-  	contenedorNuevaTarea.appendChild(tarroBasura);
-  	nuevaTarea.appendChild(contenedorNuevaTarea);
-  	nuevaTarea.setAttribute("class", "laNuevaTarea");
-  	contenedorDeTareas.appendChild(nuevaTarea);
+  contenedorNuevaTarea = document.createElement("p");
+  contenedorNuevaTarea.setAttribute("class", "pDeTarea");
+  tarroBasura = document.createElement("span");
+  tarroBasura.setAttribute("class", "icon-bin basurero");
+  //tarroBasura.setAttribute("onclick", borrarTareas());
+  nodoTarea = document.createTextNode(tareaUsuario);
+  contenedorNuevaTarea.appendChild(nodoTarea);
+  contenedorNuevaTarea.appendChild(tarroBasura);
+  nuevaTarea.appendChild(contenedorNuevaTarea);
+  nuevaTarea.setAttribute("class", "laNuevaTarea");
+  contenedorDeTareas.appendChild(nuevaTarea);
+
+  var minicaja = document.createElement('input');
+  minicaja.type = "checkbox";
+ 
+  minicaja.setAttribute("class", "cuadrito");
+  contenedorNuevaTarea.appendChild(minicaja);
+
 }
 
 /*function borrarTareas(){
